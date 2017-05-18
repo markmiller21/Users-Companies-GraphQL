d57c0d7337ecd0713090b6558155498532d1165d
Mark Miller <mark.kilpatrick.miller@gmail.com>
2017-05-18 19:07:39 -0400

    setup user and company models in graphQL

--- schema/schema.js
+++ schema/schema.js
@@ -1,25 +1,45 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const {
 	GraphQLObjectType,
 	GraphQLString,
-	GraphQLInt
+	GraphQLInt,
+	GraphQLSchema,
+	GraphQLList
 } = graphql;
 
-const users = [
-	{id: 1, firstName: "Mark", age: 25},
-	{id: 2, firstName: "Maggie", age: 24},
-	{id: 3, firstName: "Karl", age: 36}
-];
+const CompanyType = new GraphQLObjectType({
+	name: "Company",
+	fields: () => ({
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString },
+		users: {
+			type: new GraphQLList(UserType),
+			resolve(parentValue, args) {
+				console.log(parentValue, args);
+				return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+					.then(res => res.data);
+			}
+		}
+	})
+});
 
 const UserType = new GraphQLObjectType({
 	name: 'User',
-	fields: {
+	fields: () => ({
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
-		age: { type: GraphQLInt }
-	}
-})
+		age: { type: GraphQLInt },
+		company: {
+			type: CompanyType,
+			resolve(parentValue, args) {
+				return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+					.then(resp => resp.data);
+			}
+		}
+	})
+});
 
 const RootQuery = new GraphQLObjectType({
 	name: 'RootQueryType',
@@ -28,8 +48,22 @@ const RootQuery = new GraphQLObjectType({
 			type: UserType,
 			args: { id: { type: GraphQLString } },
 			resolve(parentValue, args) {
-				return _.find(users, { id: args.id });
+				return axios.get(`http://localhost:3000/users/${args.id}`)
+					.then(resp => resp.data);
+			}
+		},
+		company: {
+			type: CompanyType,
+			args: { id: { type: GraphQLString } },
+			resolve(parentValue, args) {
+				return axios.get(`http://localhost:3000/companies/${args.id}`)
+					.then(resp => resp.data);
 			}
 		}
 	}
-})
+});
+
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
